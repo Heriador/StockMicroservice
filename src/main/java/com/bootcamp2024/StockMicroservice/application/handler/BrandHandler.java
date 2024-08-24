1a9aa9b7a03ae7317f9ff92be8cc8766a61cffe0
Angel Miranda <mirandaangel83@gmail.com>
2024-08-24 12:20:35 -0500

    Finished HU4, with test and documentation

--- src/main/java/com/bootcamp2024/StockMicroservice/application/handler/BrandHandler.java
+++ src/main/java/com/bootcamp2024/StockMicroservice/application/handler/BrandHandler.java
@@ -1,11 +1,14 @@
 package com.bootcamp2024.StockMicroservice.application.handler;
 
 import com.bootcamp2024.StockMicroservice.application.dto.AddBrand;
+import com.bootcamp2024.StockMicroservice.application.dto.BrandPaginationResponse;
 import com.bootcamp2024.StockMicroservice.application.dto.BrandResponse;
+import com.bootcamp2024.StockMicroservice.application.mapper.BrandPaginationResponseMapper;
 import com.bootcamp2024.StockMicroservice.application.mapper.BrandRequestMapper;
 import com.bootcamp2024.StockMicroservice.application.mapper.BrandResponseMapper;
 import com.bootcamp2024.StockMicroservice.domain.api.IBrandServicePort;
 import com.bootcamp2024.StockMicroservice.domain.model.Brand;
+import com.bootcamp2024.StockMicroservice.domain.model.BrandPaginationCustom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,7 @@ public class BrandHandler implements IBrandHandler{
     private final IBrandServicePort brandServicePort;
     private final BrandRequestMapper brandRequestMapper;
     private final BrandResponseMapper brandResponseMapper;
+    private final BrandPaginationResponseMapper brandPaginationResponseMapper;
 
 
 
@@ -34,4 +38,14 @@ public class BrandHandler implements IBrandHandler{
 
         return brandResponseMapper.toResponse(brand);
     }
+
+    @Override
+    public BrandPaginationResponse getAllBrands(int page, int size, boolean ord) {
+
+        BrandPaginationCustom brandPaginationResponse = brandServicePort.getAllaBrands(page, size, ord);
+
+        return brandPaginationResponseMapper.toPaginationResponse(brandPaginationResponse);
+    }
+
+
 }
--- src/main/java/com/bootcamp2024/StockMicroservice/infrastructure/output/mysql/adapters/BrandAdapter.java
+++ src/main/java/com/bootcamp2024/StockMicroservice/infrastructure/output/mysql/adapters/BrandAdapter.java
@@ -1,12 +1,19 @@
 package com.bootcamp2024.StockMicroservice.infrastructure.output.mysql.adapters;
 
 import com.bootcamp2024.StockMicroservice.domain.model.Brand;
+import com.bootcamp2024.StockMicroservice.domain.model.BrandPaginationCustom;
 import com.bootcamp2024.StockMicroservice.domain.spi.IBrandPersistencePort;
 import com.bootcamp2024.StockMicroservice.infrastructure.exception.BrandAlreadyExistsException;
 import com.bootcamp2024.StockMicroservice.infrastructure.exception.BrandNotFoundException;
+import com.bootcamp2024.StockMicroservice.infrastructure.exception.NoDataFoundException;
 import com.bootcamp2024.StockMicroservice.infrastructure.output.mysql.Mapper.BrandEntityMapper;
+import com.bootcamp2024.StockMicroservice.infrastructure.output.mysql.entity.BrandEntity;
 import com.bootcamp2024.StockMicroservice.infrastructure.output.mysql.repository.IBrandRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @RequiredArgsConstructor
 public class BrandAdapter implements IBrandPersistencePort {
@@ -29,5 +36,29 @@ public class BrandAdapter implements IBrandPersistencePort {
     public Brand getBrand(String brandName) {
 
         return brandEntityMapper.brandEntityToBrand(brandRepository.findByName(brandName).orElseThrow(BrandNotFoundException::new));
+    }
+
+    @Override
+    public BrandPaginationCustom getAllBrands(int page, int size, boolean ord) {
+        Sort sort = ord ? Sort.by("name").ascending() : Sort.by("name").descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<BrandEntity> brandEntityPage = brandRepository.findAll(pageable);
+
+        if(brandEntityPage.isEmpty()){
+            throw new NoDataFoundException("No Brands found");
+        }
+
+        BrandPaginationCustom pagination = new BrandPaginationCustom();
+        pagination.setContent(brandEntityMapper.toBrandList(brandEntityPage.getContent()));
+        pagination.setPageNumber(brandEntityPage.getNumber());
+        pagination.setPageSize(brandEntityPage.getSize());
+        pagination.setTotalElements(brandEntityPage.getTotalElements());
+        pagination.setTotalPages(brandEntityPage.getTotalPages());
+        pagination.setLast(brandEntityPage.isLast());
+
+        return pagination;
+
     }
 }
